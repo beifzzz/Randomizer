c3523a6934891739268164bfe03b48d329d0c18d
beifzzz <bekknop@gmail.com>
2024-07-12 11:46:38 +0500

    Initial Commit

---
+++ Randomizer/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Randomizer
+//
+//  Created by Arystan on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
+    @IBOutlet weak var randomValueLabel: UILabel!
+    @IBOutlet weak var getRandomNumberButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getRandomNumberButton.layer.cornerRadius = 10
+    }
+    
+    
+    @IBAction func getRandomNumberButtonTapped(_ sender: Any) {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+        
+        randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+        
+    }
+    
+}
